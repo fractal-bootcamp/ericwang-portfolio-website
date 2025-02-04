3b77bdfff5e154e7e3360263420033b8616e0e90
Eric Wang <87143658+ewang0@users.noreply.github.com>
2025-02-04 10:34:49 -0500

    add theme switch + description placeholder

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -8,6 +8,11 @@ interface ProjectsProps {
 }
 
 const Projects: React.FC<ProjectsProps> = ( { projectsArr, selected, handleProjectSelect  }) => {
+    const description = (
+        <div className='text-left pb-5'>
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </div>
+    )
 
     return (
         <div className="w-full p-5 col-span-4 border-r border-neutral-700">
@@ -17,14 +22,17 @@ const Projects: React.FC<ProjectsProps> = ( { projectsArr, selected, handleProje
                 return (
                     <button 
                         key={project.name} 
-                        className="w-full border-b border-neutral-700 text-lg flex justify-between items-center cursor-pointer"
+                        className="w-full border-b border-neutral-700 text-lg flex flex-col cursor-pointer"
                         onClick={() => handleProjectSelect(index)}
                     >
-                        <div className="flex gap-4 items-center">
-                            {/* <StarIcon className="blur-[1px]"/> */}
-                            <div className={`${index === selected && "underline"} py-6 text-2xl font-bold`}>{project.name}</div>
+                        <div className='w-full flex justify-between items-center'>
+                            <div className="flex gap-4 items-center">
+                                {/* <StarIcon className="blur-[1px]"/> */}
+                                <div className={`${index === selected && "underline"} py-6 text-2xl font-bold`}>{project.name}</div>
+                            </div>
+                            <div className="border border-neutral-700 px-3 rounded-full">{project.year}</div>
                         </div>
-                        <div className="border border-neutral-700 px-3 rounded-full">{project.year}</div>
+                        {index === selected && description}
                     </button>
                 )
             })}
