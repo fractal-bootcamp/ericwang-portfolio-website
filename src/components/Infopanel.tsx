821ad756a94638ab2c813c7f64ec965359efb503
Eric Wang <87143658+ewang0@users.noreply.github.com>
2025-02-08 12:05:13 -0500

    fix build errors, add vercel analytics and vercel CLI

--- src/components/Infopanel.tsx
+++ src/components/Infopanel.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import { Switch } from '@heroui/switch';
 
-const MoonIcon = (props) => {
+const MoonIcon = () => {
     return (
       <svg
         aria-hidden="true"
@@ -10,7 +9,6 @@ const MoonIcon = (props) => {
         role="presentation"
         viewBox="0 0 24 24"
         width="1em"
-        {...props}
       >
         <path
           d="M21.53 15.93c-.16-.27-.61-.69-1.73-.49a8.46 8.46 0 01-1.88.13 8.409 8.409 0 01-5.91-2.82 8.068 8.068 0 01-1.44-8.66c.44-1.01.13-1.54-.09-1.76s-.77-.55-1.83-.11a10.318 10.318 0 00-6.32 10.21 10.475 10.475 0 007.04 8.99 10 10 0 002.89.55c.16.01.32.02.48.02a10.5 10.5 0 008.47-4.27c.67-.93.49-1.519.32-1.79z"
@@ -20,7 +18,7 @@ const MoonIcon = (props) => {
     );
 };
   
-const SunIcon = (props) => {
+const SunIcon = () => {
     return (
       <svg
         aria-hidden="true"
@@ -29,7 +27,6 @@ const SunIcon = (props) => {
         role="presentation"
         viewBox="0 0 24 24"
         width="1em"
-        {...props}
       >
         <g fill="currentColor">
           <path d="M19 12a7 7 0 11-7-7 7 7 0 017 7z" />
--- src/components/ProjectViewer.tsx
+++ src/components/ProjectViewer.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Projects from './Projects'
 import Gallery from './Gallery'
 import { useState, useEffect } from 'react'
