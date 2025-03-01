88b3df697a9abfe5dde645e7fb9edf0d54c38eda
Eric Wang <87143658+ewang0@users.noreply.github.com>
2025-02-28 19:12:40 -0500

    update placeholder links

--- src/components/MobileInfopanel.tsx
+++ src/components/MobileInfopanel.tsx
@@ -22,9 +22,9 @@ const MobileInfopanel = () => {
             <div className='flex gap-8 p-4 border-y border-neutral-700'>
                 <h2 className={`font-bold mb-2 dark:text-neutral-400 w-[92px] shrink-0`}>EXPERIENCE</h2>
                 <div className='flex flex-col gap-2'>
-                    <ExternalLink href="mailto:info@twopixel.studio" text="info@twopixel.studio" />
-                    <ExternalLink href="https://www.linkedin.com/company/twopixel-studio/" text="LinkedIn" />
-                    <ExternalLink href="https://www.instagram.com/twopixelstudio/" text="Instagram" />
+                    <ExternalLink href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" text="Github" />
+                    <ExternalLink href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" text="LinkedIn" />
+                    <ExternalLink href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" text="CV" />
                 </div>
             </div>
             <div className="py-4 border-b border-neutral-700">
